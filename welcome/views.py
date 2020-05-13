303387ea04dab3981b8a8a57716cc53fab040c22
Hidetoshi Hirokawa <1086022+h-hirokawa@users.noreply.github.com>
2020-05-13 19:28:03 +0900

    feat: json log output

--- welcome/views.py
+++ welcome/views.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from django.shortcuts import render
 from django.conf import settings
 from django.http import HttpResponse
@@ -6,17 +7,26 @@ from django.http import HttpResponse
 from . import database
 from .models import PageView
 
+logger = logging.getLogger(__name__)
+
 # Create your views here.
 
-def index(request):
-    hostname = os.getenv('HOSTNAME', 'unknown')
-    PageView.objects.create(hostname=hostname)
 
-    return render(request, 'welcome/index.html', {
-        'hostname': hostname,
-        'database': database.info(),
-        'count': PageView.objects.count()
-    })
+def index(request):
+    hostname = os.getenv("HOSTNAME", "unknown")
+    PageView.objects.create(hostname=hostname)
+    print("print debug")
+    logger.info("info log message")
+    return render(
+        request,
+        "welcome/index.html",
+        {
+            "hostname": hostname,
+            "database": database.info(),
+            "count": PageView.objects.count(),
+        },
+    )
+
 
 def health(request):
     return HttpResponse(PageView.objects.count())
